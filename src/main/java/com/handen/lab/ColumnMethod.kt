37593afd82c711b2a2becdb6d57380c62d56658a
IvanPletinskiy <Ivan.Pliatsinski@effective-soft.com>
2021-02-14 23:08:48 +0300

    Column decoding is done

--- src/main/java/com/handen/lab/ColumnMethod.kt
+++ src/main/java/com/handen/lab/ColumnMethod.kt
@@ -6,7 +6,6 @@ class ColumnMethod : Method {
             (it in 'a'..'z') || (it in 'A'..'Z')
         }
 
-        val width = key.length
         val indexes = key.toCharArray()
                 .withIndex()
                 .sortedBy {
@@ -17,7 +16,7 @@ class ColumnMethod : Method {
 
         val encoded = indexes.map {
             var text = ""
-            for(i in it until filtered.length step key.length) {
+            for (i in it until filtered.length step key.length) {
                 text += filtered[i]
             }
             text
@@ -29,7 +28,41 @@ class ColumnMethod : Method {
     }
 
     override fun decode(text: String, key: String): String {
-        return ""
+        val filtered = text.filter {
+            (it in 'a'..'z') || (it in 'A'..'Z')
+        }
+
+        val fullColumnsCount = filtered.length % key.length
+        val columnLength = filtered.length / key.length
+
+        var startIndex = 0
+
+        val columns = Array(key.length) { "" }
+
+        key.toCharArray()
+                .withIndex()
+                .sortedBy {
+                    it.value
+                }.forEach {
+                    val takeCount = if (it.index < fullColumnsCount) {
+                        columnLength + 1
+                    } else {
+                        columnLength
+                    }
+
+                    val endIndex = startIndex + takeCount
+
+                    val columnText = filtered.substring(startIndex, endIndex)
+                    startIndex = endIndex
+                    columns[it.index] = columnText
+                }
+
+        val decoded = (filtered.indices).map {
+            val columnIndex = it % key.length
+            columns[columnIndex][it / key.length]
+        }.toCharArray()
+
+        return String(decoded)
     }
 }
 
@@ -38,4 +71,6 @@ fun main() {
     val decodedText = "cryptographylove"
     val key = "key"
     println("Encode: ${columnMethod.encode(decodedText, key)}")
+    val encoded = "rtrhocpgpleyoayv"
+    println("Decode: ${columnMethod.decode(encoded, key)}")
 }
