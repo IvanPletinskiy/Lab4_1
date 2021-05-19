f7b2784d691371af5155008742d18b65dbca3522
IvanPletinskiy <Ivan.Pliatsinski@effective-soft.com>
2021-05-19 18:14:34 +0300

    refactor

--- src/main/java/com/handen/lab/PushNotifications.kt
+++ src/main/java/com/handen/lab/PushNotifications.kt
@@ -1,9 +1,90 @@
 package com.handen.lab
 
-fun sendNow(title: String, message: String) {
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.util.*
 
+private const val DEFAULT_PORT = 4555
+
+fun sendNow(title: String, message: String, callback: OnSendNotificationCallback) {
+    Thread {
+        try {
+            val lines = readArpTable()
+            val mask = getNetworkMaskFromTable(lines)
+            if (mask != null) {
+                val broadcastAddress = getBroadcastAddress(mask)
+                val packet = createDatagramPacket(title, message, broadcastAddress)
+                val datagramSocket = createDatagramSocket()
+                datagramSocket.send(packet)
+            } else {
+                callback.onFailure("Can't get subnet mask")
+            }
+        } catch (e: Exception) {
+            callback.onFailure("Unknown error during notification delivery")
+        }
+    }.start()
 }
 
-fun sendAtTime(title: String, message: String, dateTime: Long) {
+fun sendAtTime(
+    title: String,
+    message: String,
+    dateTime: Long,
+    callback: OnSendNotificationCallback
+) {
+    Thread {
+        while (dateTime > Date().time) {
+            Thread.sleep(1)
+        }
+        sendNow(title, message, callback)
+    }.start()
+}
 
+private fun readArpTable(): List<String> {
+    val arpProcess = Runtime.getRuntime().exec("arp -a")
+    val lines = arpProcess.inputStream.bufferedReader(charset("cp866")).readLines()
+    arpProcess.destroy()
+    return lines
+}
+
+private fun getNetworkMaskFromTable(lines: List<String>): String? {
+    val dynamicTableEntries = lines.filter {
+        it.contains("динамический")
+    }
+    val addresses = dynamicTableEntries.map {
+        val array = it.trim().split("\\s+".toRegex())
+        val ipAddress = array[0]
+        ipAddress
+    }
+
+    return addresses.firstOrNull()
+}
+
+private fun getBroadcastAddress(mask: String): InetAddress {
+    val address: InetAddress = InetAddress.getByName(mask)
+    val a: ByteArray = address.address
+    a[3] = 255.toByte()
+    return InetAddress.getByAddress(a)
+}
+
+private fun createDatagramPacket(
+    title: String,
+    message: String,
+    broadcastAddress: InetAddress
+): DatagramPacket {
+    val bytes = "$title\n$message".toByteArray(Charsets.UTF_16)
+    val packet = DatagramPacket(bytes, bytes.size, broadcastAddress, DEFAULT_PORT)
+    return packet
+}
+
+private fun createDatagramSocket(): DatagramSocket {
+    val datagramSocket = DatagramSocket(DEFAULT_PORT)
+    datagramSocket.broadcast = true
+    datagramSocket.soTimeout = 5000
+    return datagramSocket
+}
+
+
+interface OnSendNotificationCallback {
+    fun onFailure(message: String)
 }
--- src/main/java/com/handen/lab/controller/MainController.java
+++ src/main/java/com/handen/lab/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.handen.lab.controller;
 
+import com.handen.lab.OnSendNotificationCallback;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -32,29 +34,79 @@ public class MainController implements Initializable {
     public TextField time_text_field;
     public Label error_label;
 
+    private OnSendNotificationCallback callback = new OnSendNotificationCallback() {
+        @Override
+        public void onFailure(String message) {
+            showError(message);
+        }
+    };
+
     public void onSendButtonClicked(ActionEvent actionEvent) {
+        error_label.setVisible(false);
         boolean sendingNow = now_check_box.isSelected();
         String title = title_text_field.getText();
         String message = message_text_field.getText();
-        if (sendingNow) {
-            sendNow(title, message);
-        } else {
-            LocalDate localDate = date_picker.getValue();
-            Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
-            Date date = Date.from(instant);
-            SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
-            String timeString = time_text_field.getText();
-            long timeMillis = -1;
-            try {
-                Date timeDate = dateFormat.parse(timeString);
-                timeMillis = timeDate.getTime();
-            } catch(ParseException exception) {
-                exception.printStackTrace();
-            }
 
-            long sendMillis = date.getTime() + timeMillis;
-            sendAtTime(title, message, sendMillis);
+        boolean result = validate_input(title, message);
+
+        if(result) {
+            if(sendingNow) {
+                sendNow(title, message, callback);
+            }
+            else {
+                boolean validate_date_result = validate_date_input();
+                if(validate_date_result) {
+                    sendWithDelay(title, message);
+                }
+                else {
+                    showError("Некорректный ввод времени отправки");
+                }
+            }
         }
+        else {
+            showError("Некорректный ввод");
+        }
+    }
+
+    private void sendWithDelay(String title, String message) {
+        Date date = getDatepickerDate();
+        long timeMillis = parseTime();
+
+        long sendMillis = date.getTime() + timeMillis;
+        sendAtTime(title, message, sendMillis, callback);
+    }
+
+    private Date getDatepickerDate() {
+        LocalDate localDate = date_picker.getValue();
+        Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+        return Date.from(instant);
+    }
+
+    private long parseTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        String timeString = time_text_field.getText();
+        long timeMillis = -1;
+        try {
+            Date timeDate = dateFormat.parse(timeString);
+            timeMillis = timeDate.getTime();
+        }
+        catch(ParseException exception) {
+            exception.printStackTrace();
+        }
+        return timeMillis;
+    }
+
+    private boolean validate_date_input() {
+        return true;
+    }
+
+    private boolean validate_input(String title, String message) {
+        return true;
+    }
+
+    private void showError(String message) {
+        error_label.setText(message);
+        error_label.setVisible(true);
     }
 
     @Override
@@ -75,183 +127,4 @@ public class MainController implements Initializable {
         select_time_label.setVisible(isVisible);
         time_text_field.setVisible(isVisible);
     }
-
-    //    private static final String INITIAL_KEY = "11111111111111111111111111111111111";
-//    public TextArea keyTextView;
-//    private String decodedPath = "C:\\ti\\lfsr\\decoded.txt";
-//    private String encodedPath = "C:\\ti\\lfsr\\encoded.txt";
-//    public AnchorPane container;
-//    public Label decodedFilePath;
-//    public Label encodedFilePath;
-//    public TextArea inputTextView;
-//    public TextField initialRegisterValue;
-//    public TextArea resultTextView;
-//    private Stage stage;
-//
-//    public Label errorLabel;
-//
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        initialRegisterValue.setText(INITIAL_KEY);
-//        initialRegisterValue.setTextFormatter(new BinaryTextFormatter());
-//        updateDecodedPath();
-//        updateEncodedPath();
-//        errorLabel.setWrapText(true);
-//    }
-//
-//    public void setStage(Stage stage) {
-//        this.stage = stage;
-//    }
-//
-//    public void onEncodeClicked(MouseEvent mouseEvent) {
-//        boolean isValid = validateKey();
-//        if(isValid) {
-//            LFSR lfsr = new LFSR(initialRegisterValue.getText());
-//            byte[] bytes = readBytesFromPath(decodedPath);
-//            byte[] encodedBytes = lfsr.encodeBytes(bytes);
-//            StringBuilder bytesString = new StringBuilder();
-//            for(int i = 0; i < bytes.length && i < 1000; i++) {
-//                bytesString.append(String.format("%8s", Integer.toBinaryString(bytes[i] & 0xFF)).replace(' ', '0'));
-//            }
-//            inputTextView.setText(bytesString.toString());
-//
-//            StringBuilder decodedBytesString = new StringBuilder();
-//            for(int i = 0; i < encodedBytes.length && i < 1000; i++) {
-//                decodedBytesString.append(String.format("%8s", Integer.toBinaryString(encodedBytes[i] & 0xFF)).replace(' ', '0'));
-//            }
-//            resultTextView.setText(decodedBytesString.toString());
-//            writeBytesToPath(encodedBytes, encodedPath);
-//            keyTextView.setText(lfsr.getKey());
-//        }
-//        else {
-//            inputTextView.setText("");
-//            resultTextView.setText("");
-//        }
-//    }
-//
-//    public void onDecodeClicked(MouseEvent mouseEvent) {
-//        boolean isValid = validateKey();
-//        if(isValid) {
-//            LFSR lfsr = new LFSR(initialRegisterValue.getText());
-//            byte[] bytes = readBytesFromPath(encodedPath);
-//            byte[] decodedBytes = lfsr.decodeBytes(bytes);
-//            StringBuilder bytesString = new StringBuilder();
-//            for(int i = 0; i < bytes.length && i < 1000; i++) {
-//                bytesString.append(String.format("%8s", Integer.toBinaryString(bytes[i] & 0xFF)).replace(' ', '0'));
-//            }
-//            inputTextView.setText(bytesString.toString());
-//
-//            StringBuilder decodedBytesString = new StringBuilder();
-//            for(int i = 0; i < decodedBytes.length && i < 1000; i++) {
-//                decodedBytesString.append(String.format("%8s", Integer.toBinaryString(decodedBytes[i] & 0xFF)).replace(' ', '0'));
-//            }
-//            resultTextView.setText(decodedBytesString.toString());
-//            writeBytesToPath(decodedBytes, decodedPath);
-//            keyTextView.setText(lfsr.getKey());
-//        }
-//        else {
-//            inputTextView.setText("");
-//            resultTextView.setText("");
-//        }
-//    }
-//
-//    private boolean validateKey() {
-//        String key = initialRegisterValue.getText();
-//        boolean isValid = true;
-//        if(key.length() != 35) {
-//            errorLabel.setText("Ошибка: Длина ключа должна быть 35 символов, сейчас: " + key.length());
-//            isValid = false;
-//        }
-//
-//        if(isValid) {
-//            errorLabel.setVisible(false);
-//            errorLabel.setText("");
-//        }
-//        else {
-//            errorLabel.setVisible(true);
-//        }
-//
-//        return isValid;
-//    }
-//
-//    private byte[] readBytesFromPath(String path) {
-//        FileInputStream fileInputStream = null;
-//        File file = new File(path);
-//        try {
-//            fileInputStream = new FileInputStream(file);
-//            return fileInputStream.readAllBytes();
-//        }
-//        catch(IOException e) {
-//            e.printStackTrace();
-//            return new byte[0];
-//        }
-//        finally {
-//            if(fileInputStream != null) {
-//                try {
-//                    fileInputStream.close();
-//                }
-//                catch(IOException exception) {
-//                    exception.printStackTrace();
-//                }
-//            }
-//        }
-//    }
-//
-//    private void writeBytesToPath(byte[] bytes, String path) {
-//        File file = new File(path);
-//        FileOutputStream fileOutputStream = null;
-//        try {
-//            fileOutputStream = new FileOutputStream(file);
-//            fileOutputStream.write(bytes);
-//        }
-//        catch(IOException exception) {
-//            exception.printStackTrace();
-//        }
-//        finally {
-//            if(fileOutputStream != null) {
-//                try {
-//                    fileOutputStream.close();
-//                }
-//                catch(IOException exception) {
-//                    exception.printStackTrace();
-//                }
-//            }
-//        }
-//    }
-//
-//    public void onOpenDecodedFile(ActionEvent actionEvent) {
-//        File file = chooseFile("Open decoded file");
-//        if(file != null) {
-//            decodedPath = file.getAbsolutePath();
-//            updateDecodedPath();
-//        }
-//    }
-//
-//    public void onOpenEncodedFile(ActionEvent actionEvent) {
-//        File file = chooseFile("Open encoded file");
-//        if(file != null) {
-//            encodedPath = file.getAbsolutePath();
-//            updateEncodedPath();
-//        }
-//    }
-//
-//    private File chooseFile(String title) {
-//        FileChooser fileChooser = new FileChooser();
-//        fileChooser.setTitle(title);
-//        String userDirectoryString = System.getProperty("user.home");
-//        File userDirectory = new File(userDirectoryString);
-//        if(!userDirectory.canRead()) {
-//            userDirectory = new File("c:/");
-//        }
-//        fileChooser.setInitialDirectory(userDirectory);
-//        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("*", "*.*"));
-//        return fileChooser.showOpenDialog(stage);
-//    }
-//
-//    private void updateDecodedPath() {
-//        decodedFilePath.setText("Расшифрованный файл:" + decodedPath);
-//    }
-//
-//    private void updateEncodedPath() {
-//        encodedFilePath.setText("Зашифрованный файл:" + encodedPath);
-//    }
 }
